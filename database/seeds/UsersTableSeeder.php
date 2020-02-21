de075f3f560eb12f7448e45173342fd2b63b9a53
Lateisha Austin <lqaustin@coastal.edu>
2020-02-21 09:05:34 -0500

    Updated Users seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,9 +1,8 @@
 <?php
 
-//Load Hotel Model
-use App\Models\Hotel;
-
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,48 +13,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // array of specific hotels to populate database
-        $hotels = [
-            [
-                'name' => 'Port Orleans French Quarter',
-                'address_1' => '2201 Orleans Drive',
-                'city' => 'Orlando',
-                'state' => 'FL',
-                'zip' => '32830',
-                'description' => 'International luxurious hotel.',
-                'image' => 'https://placeimg.com/640/480/arch'
-            ],
-            [
-                'name' => 'Compass Cove Ocean Front Resort',
-                'address_1' => '2311 S Ocean Blvd',
-                'city' => 'Myrtle Beach',
-                'state' => 'SC',
-                'zip' => '29577',
-                'description' => 'Luxurious ocean front hotel.',
-                'image' => 'https://placeimg.com/640/480/arch'
-            ],
-            [
-                'name' => 'The Embassy Row Hotel',
-                'address_1' => '2015 Massachusetts Ave NW',
-                'city' => 'Washington',
-                'state' => 'DC',
-                'zip' => '20036',
-                'description' => 'International luxurious hotel.',
-                'image' => 'https://placeimg.com/640/480/arch'
-            ]
-        ];
-
-        //Loop through hotels array and create a hotel using the hotel model and mass assignment
-        foreach ($hotels as $hotel) {
-            Hotel::create(array(
-                'name' => $hotel['name'],
-                'address_1' => $hotel['address_1'],
-                'city' => $hotel['city'],
-                'state' => $hotel['state'],
-                'zip' => $hotel['zip'],
-                'description' => $hotel['description'],
-                'image' => $hotel['image']
-            ));
-        }
+        DB::table('users')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
     }
 }
