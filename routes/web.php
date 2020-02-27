8ea61eab0c52221c4a6af8d90db1e186720a4b9f
Lateisha Austin <lqaustin@coastal.edu>
2020-02-26 20:06:29 -0500

    added get routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+	return '/ route';
+});
+
+Route::get(‘route’, function() {
+	return ‘/hotels route’;
+});
+
+Route::group([‘prefix’ => ‘reservations’], function(){
+	Route::get(‘/’, function(){ return 'Showing users homepage';}); 
+	Route::get(‘reservations’, function(){ return 'Showing users reservations';});
+	Route::get(‘reservations/new’, function(){ return 'Showing form to create reservations';});
+	Route::get(‘reservations/{id}’, function(){ return 'Showing reservation id';});
+	Route::get(‘reservations/{id}/edit’, function(){ return 'Showing edit form for reservation id';});
+	Route::post(‘reservations’, function() { return ‘Creating reservation’; });
+	Route::put(‘reservations/{id}’, function() { return ‘Updating reservation id’; });
+	Route::delete(‘reservations/{id}’, function() { return ‘deleting reservation id’; });
 });
